90c97502e255e2b9e425501046fe8d55d17ad4c9
fajar.farisya <fajar.farisya@pactindo.com>
2025-01-20 11:00:00 +0700

    module 8 - useRoute dan useRouter

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/posts">Posts</RouterLink>
   </nav>
 
   <router-view v-slot="{ Component }">
